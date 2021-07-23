2892f185a9a980ee798c7f36d838b4d2c5477ead
Glitch (systemv2ok) <none>
2021-07-23 17:32:09 +0000

    😚☺️ Checkpoint ./server.js:32281840/21

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const hastebins = require("hastebin-gen");
 const getYoutubeID = require("get-youtube-id");
 const yt_api_key = "AIzaSyAXaeBh837k38o_lwSADet8UTO7X21DGsY"; ///تعديل اساسي سوي اي بي اي جديد
 const pretty = require("pretty-ms");
-client.login(process.env.TOKEN);
+client.login("");
 const queue = new Map();
 var table = require("table").table;
 const Discord = require("discord.js");
