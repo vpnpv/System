77fa7a396dfb00215f31fb870f7386dced0485b2
Glitch (systemv2ok) <none>
2022-03-14 11:30:02 +0000

    🚔🌏 Checkpoint ./server.js:25146431/294 ./config.js:25146431/38

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ const { get } = require("snekfetch");
 const guild = require("guild");
 const dateFormat = require("dateformat");
 const YouTube = require("simple-youtube-api");
-const youtube = new YouTube("AIzaSyAXaeBh837k38o_lwSADet8UTO7X21DGsY"); //تعديل اساسي سوي اي بي اي جديد
+const youtube = new YouTube("AIzaSyDGW8m0K6tXlubW7QEXrjyroFXGrQY_gkw"); //تعديل اساسي سوي اي بي اي جديد
 const hastebins = require("hastebin-gen");
 const getYoutubeID = require("get-youtube-id");
-const yt_api_key = "AIzaSyAXaeBh837k38o_lwSADet8UTO7X21DGsY"; ///تعديل اساسي سوي اي بي اي جديد
+const yt_api_key = "AIzaSyDGW8m0K6tXlubW7QEXrjyroFXGrQY_gkw"; ///تعديل اساسي سوي اي بي اي جديد
 const pretty = require("pretty-ms");
-client.login("ODU4MTU0NjgyMjg2NzM1MzY3.YNaA1A.yr2VZoO4p78syz9J5afOfts6iLA");///توكن البوت 
+client.login(process.env.TOKEN);///توكن البوت 
 const queue = new Map();
 var table = require("table").table;
 const Discord = require("discord.js");
@@ -53,7 +53,7 @@ client.on("ready", () => {
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   console.log(client.guilds.map(c => `${c.name} : ${c.me.hasPermission(8)}`));
-  client.user.setStatus("online");
+  client.user.setStatus("dnd");
 
   client.user.setActivity(`${prefix}help`, { type: "WATCHING" });
 });
@@ -227,7 +227,7 @@ client.on("message", message => {
       .addField("**Channels** : ", `» ${client.channels.size} `, true)
       .addField("**Users** : ", `» ${client.users.size} `, true)
       .addField("**Bot Name** :  ", `» ${client.user.tag} `, true)
-      .addField("**Bot Owner** :  ", `» <@>697179839962153090`, true) // تعديل اساسي غير الايدي لايدي حسابك
+      .addField("**Bot Owner** :  ", `» <@!711293810424152085>`, true) // تعديل اساسي غير الايدي لايدي حسابك
       .setImage("")
       .setFooter(message.author.username, message.client.avatarURL);
     message.channel.send(bot);
